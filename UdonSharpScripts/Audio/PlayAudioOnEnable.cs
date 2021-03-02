3efb9aa9c6d6336d6942332bdb6c2604abae61dc
kurotori4423 <kurotori4423@gmail.com>
2021-03-03 02:09:47 +0900

    Mirrorのボタンの音が出るように変更 SimpleChairを追加 その他Prefabを使いやすいように調整

---
+++ UdonSharpScripts/Audio/PlayAudioOnEnable.cs
@@ -0,0 +1,20 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+/// <summary>
+/// オブジェクトがアクティブになった時に一回音声を再生する
+/// （2021/03/03現在 エディタ上では最初から非アクティブのオブジェクトでは動きません。）
+/// </summary>
+public class PlayAudioOnEnable : UdonSharpBehaviour
+{
+    [SerializeField]
+    AudioSource audioSorce;
+
+    private void OnEnable()
+    {
+        audioSorce.Play();
+    }
+}
---
+++ UdonSharpScripts/SimpleChair.cs
@@ -0,0 +1,17 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+/// <summary>
+/// シンプルな椅子用スクリプト
+/// 最初から非アクティブなVRC Chairにつけておくと、アクティブ時に有効にならない問題を回避できる。
+/// </summary>
+public class SimpleChair : UdonSharpBehaviour
+{
+    public override void Interact()
+    {
+        Networking.LocalPlayer.UseAttachedStation();
+    }
+}
